4796623db29ca7e5f5fc2d5e9abb88b05219133f
Felipe Leao <flpleao1807@gmail.com>
2021-05-13 05:33:31 -0300

    fix - Attribute length

--- livros.h
+++ livros.h
@@ -10,13 +10,15 @@
 #include <ctype.h>
 #include "arquivos.h"
 
-#define MAX_ATRIBUTO_LIVRO 60
+#define MAX_ATRIBUTO_LIVRO 40
+#define MAX_TAMANHO_GENERO 20
+
 
 //DESCRICAO A MOSTRAR PARA O MUNDO;
 typedef struct livro
 {
     char nome[MAX_ATRIBUTO_LIVRO];
-    char genero[MAX_ATRIBUTO_LIVRO];
+    char genero[MAX_TAMANHO_GENERO];
     char autor[MAX_ATRIBUTO_LIVRO];
     char editora[MAX_ATRIBUTO_LIVRO];
     int codigo;
