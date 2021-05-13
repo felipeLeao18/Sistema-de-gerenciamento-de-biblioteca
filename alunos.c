c0182cf33039c6c606aee1bc9dff7b733e3b3b30
Felipe Leao <flpleao1807@gmail.com>
2021-05-13 09:35:00 -0300

    feature -alteraDados

--- alunos.c
+++ alunos.c
@@ -8,6 +8,7 @@ int retornaQntAlunosEmArquivo(FILE *file, size_t tamanhoDoArquivo);
 void procuraOcorrencia(bool encontraOcorrencia, int registroAcademico);
 void mostraAluno(Aluno *aluno);
 void liberaListaDeAlunos(Aluno **aluno);
+void inputAluno(Aluno *aluno);
 
 Aluno *alocaMemoriaParaListaDeAlunos(int quantidadeDeAlunos)
 {
@@ -29,6 +30,18 @@ void tratamentoDeErroAlocacaoAlunos(Aluno *aluno)
 void liberaListaDeAlunos(Aluno **aluno)
 {
     free(&aluno);
+}
+
+void inputAluno(Aluno *aluno)
+{
+    printf("Digite o nome do aluno\n");
+    scanf(" %[^\n]", &aluno->nomeCompleto);
+
+    printf("Digite o registro academico do aluno\n");
+    scanf("%d", &aluno->registroAcademico);
+
+    printf("Digite o numero de celular do aluno:\n");
+    scanf("%ld", &aluno->numeroCelular);
 }
 
 void preencheListaDeAlunosComArquivo(Aluno *aluno, FILE *file, Aluno *arrayAlunos)
@@ -81,15 +94,7 @@ void adicionarAluno()
     for (index = 0; index < quantidadeDeAlunosParaCadastro; index++)
     {
 
-        printf("Digite o nome do aluno\n");
-        scanf(" %[^\n]", &arrayAlunos[index].nomeCompleto);
-
-        printf("Digite o registro academico do aluno\n");
-        scanf("%d", &arrayAlunos[index].registroAcademico);
-
-        printf("Digite o numero de celular do aluno:\n");
-        scanf("%ld", &arrayAlunos[index].numeroCelular);
-
+        inputAluno(&arrayAlunos[index]);
         fwrite(&arrayAlunos[index], sizeof(arrayAlunos[index]), 1, file);
         printf("\nAluno '%s' cadastrado com sucesso!!!\n", arrayAlunos[index].nomeCompleto);
     }
@@ -162,10 +167,31 @@ void buscaAlunoRegistroAcademico()
     getch();
 }
 
-void alterarAluno()
+void alteraDadosAluno()
 {
-    FILE *file = abreArquivo("arquivoAlunos.dat", "rb");
-    FILE *fileTemporario = abreArquivo("arquivoTemporario.dat", "wb");
+    FILE *file = abreArquivo("arquivoAlunos.dat", "rb+");
+    Aluno aluno;
+    bool achei = false;
+    int buscaRegistroAcademico;
+    printf("Digite o Registro academico do aluno que voce deseja atualizar os dados:\n");
+    scanf("%d", &buscaRegistroAcademico);
+
+    while (fread(&aluno, sizeof(Aluno), 1, file) > 0)
+    {
+        if (aluno.registroAcademico == buscaRegistroAcademico)
+        {
+            printf("Registro encontrado, aluno: %s, RA: %d; Digite os novos valores para os dados do aluno\n", aluno.nomeCompleto, aluno.numeroCelular);
+            achei = true;
+            inputAluno(&aluno);
+            fseek(file, -sizeof(Aluno), SEEK_CUR);
+            fwrite(&aluno, sizeof(Aluno), 1, file);
+            fclose(file);
+            printf("Registro alterado com sucesso!\n");
+        }
+    }
+    procuraOcorrencia(achei, buscaRegistroAcademico);
+    printf("\nPressione qualquer tecla para continuar:\n");
+    getch();
 }
 
 void excluirAluno()
@@ -173,7 +199,7 @@ void excluirAluno()
     listarTodosAlunos();
     printf("\n\n");
     FILE *file = abreArquivo("arquivoAlunos.dat", "rb");
-    FILE *fileTemporario = abreArquivo("arquivoTemporario.dat", "wb");
+    FILE *fileTemporario = abreArquivo("arquivoAlunosTemporario.dat", "wb");
     size_t tamanhoDoArquivo = retornaTamanhoArquivo(file);
 
     Aluno aluno;
@@ -206,7 +232,7 @@ void excluirAluno()
     fclose(file);
     fclose(fileTemporario);
     liberaListaDeAlunos(&arrayAlunos);
-    renomeiaArquivoTemporario("arquivoAlunos.dat", "arquivoTemporario.dat");
+    renomeiaArquivoTemporario("arquivoAlunos.dat", "arquivoAlunosTemporario.dat");
 
     printf("\n\nPressione qualquer tecla para continuar!\n");
     getch();
