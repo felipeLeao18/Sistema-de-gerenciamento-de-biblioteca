9f05af7ec6282f10b095e39a518f93ee90ce07b6
Felipe Leao <flpleao1807@gmail.com>
2021-05-21 21:47:27 -0300

    final version

--- sistema.c
+++ sistema.c
@@ -1,6 +1,6 @@
 #include "sistema.h"
 
-int comparaNomesLivro(const char *original, const char *buscada)
+int comparaString(const char *original, const char *buscada)
 {
     unsigned int tamanhoStrOriginal = strlen(original);
     if (strlen(buscada) != tamanhoStrOriginal)
@@ -28,8 +28,33 @@ void tiraLivroEstoque(char *nomeLivro)
             fclose(file);
         }
     }
+}
 
-    //TODO VERIFICAOCORRENCIA;
+int verificaOcorrenciaRegistroAcademico(char *registroAcademico)
+{
+    FILE *file = abreArquivo("arquivoSistema.dat", "rb");
+    Cliente cliente;
+    bool achei = false;
+
+    while (fread(&cliente, sizeof(Cliente), 1, file))
+    {
+        if (comparaString(cliente.aluno.registroAcademico, registroAcademico))
+        {
+            fseek(file, -sizeof(Cliente), SEEK_CUR);
+            achei = true;
+            break;
+        }
+    }
+    if (achei)
+    {
+        printf("O aluno '%s' com Registro '%s' ja possui alugueis pendentes\n", cliente.aluno.nomeCompleto, cliente.aluno.registroAcademico);
+        return 1;
+    }
+    else
+    {
+
+        return 0;
+    }
 }
 
 Livro retornaLivro(char *nome)
@@ -40,7 +65,7 @@ Livro retornaLivro(char *nome)
 
     while (fread(&livro, sizeof(Livro), 1, file) > 0)
     {
-        if (comparaNomesLivro(livro.nome, nome))
+        if (comparaString(livro.nome, nome))
         {
             achei = true;
 
@@ -54,9 +79,14 @@ Livro retornaLivro(char *nome)
     {
         return livro;
     }
+    else
+    {
+        printf("Livro nao encontrado, confira os dados e tente novamente!\n");
+        exit(1);
+    }
 }
 
-Aluno retornaAluno(int registroAcademico)
+Aluno retornaAluno(char *registroAcademico)
 {
 
     FILE *file = abreArquivo("arquivoAlunos.dat", "rb");
@@ -65,7 +95,7 @@ Aluno retornaAluno(int registroAcademico)
 
     while (fread(&aluno, sizeof(Aluno), 1, file) > 0)
     {
-        if (aluno.registroAcademico == registroAcademico)
+        if (comparaString(aluno.registroAcademico, registroAcademico))
         {
 
             achei = true;
@@ -81,40 +111,108 @@ Aluno retornaAluno(int registroAcademico)
     {
         return aluno;
     }
+    else
+    {
+        printf("Aluno nao encontrado no sistema, confira os dados e tente novamente!\n");
+        exit(1);
+    }
+}
+bool verificaDisponibilidade(char *nomeLivro)
+{
+    FILE *file = abreArquivo("arquivoLivros.dat", "rb");
+    Livro livro;
+    bool achei = false;
+
+    while (fread(&livro, sizeof(Livro), 1, file) > 0)
+    {
+        if (comparaString(livro.nome, nomeLivro))
+        {
+            achei = true;
+
+            fseek(file, -sizeof(Livro), SEEK_CUR);
+            break;
+        }
+    }
+
+    fclose(file);
+    if (livro.quantidade >= 1)
+    {
+        return true;
+    }
+    else
+    {
+        printf("O livro '%s' nao possui quantidade suficiente para aluguel!\n", nomeLivro);
+        return false;
+    }
+}
+
+bool inputCliente(Cliente *cliente)
+{
+    int index;
+    
+
+    printf("Digite o Registro academico do aluno que ira alugar o(s) livro(s):\n");
+    scanf(" %[^\n]", cliente->aluno.registroAcademico);
+    cliente->aluno = retornaAluno(cliente->aluno.registroAcademico);
+
+    if (verificaOcorrenciaRegistroAcademico(cliente->aluno.registroAcademico) == 1)
+    {
+        printf("Confira os dados e tente novamente!\n");
+        return false;
+    }
+
+    printf("Digite a quantidade de livros que o aluno ira alugar:\n");
+    scanf("%d", &cliente->quantidadeLivros);
+
+    for (index = 0; index < cliente->quantidadeLivros; index++)
+    {
+        printf("Digite o nome do(s) livro(s) a serem alugados:\n");
+        scanf(" %[^\n]", &cliente->livro[index].nome);
+        if (verificaDisponibilidade(cliente->livro[index].nome))
+        {
+
+            cliente->livro[index] = retornaLivro(cliente->livro[index].nome);
+            tiraLivroEstoque(cliente->livro[index].nome);
+        }
+        else
+        {
+            printf("Quantidade para livro '%s' indisponivel para aluguel\n", cliente->livro[index].nome);
+            return false;
+        }
+    }
+    return true;
 }
 
 void criaCliente()
 {
-    int index;
-
     FILE *file = abreArquivo("arquivoSistema.dat", "ab");
     Cliente *cliente;
     cliente = (Cliente *)malloc(sizeof(Cliente));
+    if (inputCliente(cliente))
+    {
 
-    cliente->aluno = retornaAluno(7);
+        fwrite(cliente, sizeof(Cliente), 1, file);
+        fclose(file);
 
-    cliente->livro[0] = retornaLivro("clean coda");
-
-    tiraLivroEstoque("clean coda");
-
-    cliente->livro[1] = retornaLivro("matematica basica");
-
-    tiraLivroEstoque("Matematica basica");
-
-    fwrite(cliente, sizeof(Cliente), 1, file);
-    fclose(file);
-    free(&cliente);
+        free(&cliente);
+    }
 }
 
-void buscaCliente(int registroAcademico)
+void buscaCliente()
 {
     FILE *file = abreArquivo("arquivoSistema.dat", "rb");
     Cliente cliente;
+
     bool achei = false;
-    printf("ue");
+    int index;
+
+    char registroAcademico[19];
+    printf("Digite o RA do aluno a ser buscado\n");
+    scanf(" %[^\n]", registroAcademico);
+
     while (fread(&cliente, sizeof(Cliente), 1, file) > 0)
     {
-        if (cliente.aluno.registroAcademico == registroAcademico)
+        if (comparaString(cliente.aluno.registroAcademico, registroAcademico))
 
         {
 
@@ -124,44 +222,17 @@ void buscaCliente(int registroAcademico)
 
             printf("\n- - - - - - - - - - - - - - - - - -\n");
 
-            printf("NOME ALUNO: %s\n", cliente.aluno.nomeCompleto);
-            printf("NOME LIVRO 1: %s\n", cliente.livro[0].nome);
-            printf("EDITORA LIVRO 1: %s\n", cliente.livro[0].editora);
+            printf("NOME ALUNO: %s\n\n", cliente.aluno.nomeCompleto);
 
-            printf("NOME LIVRO 2: %s\n", cliente.livro[1].nome);
-            printf("EDITORA LIVRO 2: %s\n", cliente.livro[1].editora);
+            printf("Livro(s) alugado(s):\n");
+            for (index = 0; index < cliente.quantidadeLivros; index++)
+            {
+                printf("%s, autor: %s, editora: %s\n", cliente.livro[index].nome, cliente.livro[index].autor, cliente.livro[index].editora);
+            }
 
             printf("- - - - - - - - - - - - - - - - - -\n");
 
             fclose(file);
         }
-    }
-}
-
-bool verificaDisponibilidade(char *nomeLivro)
-{
-    FILE *file = abreArquivo("arquivoLivros.dat", "rb+");
-    Livro livro;
-    bool achei = false;
-
-    char nomeLivroAlterado[MAX_ATRIBUTO_LIVRO];
-    printf("Digite o nome do livro a ser alterado:\n");
-    scanf(" %[^\n]", nomeLivroAlterado);
-
-    while (fread(&livro, sizeof(Livro), 1, file) > 0)
-    {
-        if (comparaString(livro.nome, nomeLivroAlterado))
-        {
-            achei = true;
-            printf("Registro encontrado! Digite os novos dados do livro: '%s' abaixo:\n\n", livro.nome);
-
-            fseek(file, -sizeof(Livro), SEEK_CUR);
-        }
-    }
-
-    fclose(file);
-    if (livro.quantidade >= 1)
-    {
-        return true;
     }
 }
